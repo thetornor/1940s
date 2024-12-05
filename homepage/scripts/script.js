60e0b2a9aba4facd8dae2b10cae002c9afa88230
thetornor <duangpawan15745@gmail.com>
2024-12-05 00:52:15 -0500

    thu5

---
+++ homepage/scripts/script.js
@@ -0,0 +1,17 @@
+let grid_items = document.querySelectorAll('.grid_item');
+let length = grid_items.length;
+
+for(let i = 0; i < length; i++){
+    let row_id, col_id;
+
+    if(i != (length-1)){
+        row_id = Math.floor(i/3);
+        col_id = i%3;
+    }else{
+        row_id = 4;
+        col_id = 2;
+    }
+
+    // insert box_** into grid item class
+    grid_items[i].classList.add(`box_${row_id}${col_id}`);
+}
