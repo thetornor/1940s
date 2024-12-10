fe130012266eeaecb22776d99efeed2c8f3cc5be
thetornor <duangpawan15745@gmail.com>
2024-12-10 13:34:30 -0500

    tue10

--- entry10/script.js
+++ entry10/script.js
@@ -1,11 +1,17 @@
-const showAlertBtn = document.getElementById('showAlert');
-const customAlert = document.getElementById('customAlert');
-const confirmBtn = document.getElementById('confirmBtn');
+let currentImageIndex = -1;
 
-showAlertBtn.addEventListener('click',function(){
-    customAlert.style.display='flex';  
-});
+function getRandomImage() {
+    let newImageIndex;
 
-confirmBtn.addEventListener('click',function(){
-    customAlert.style.display='none';
+    do {
+        newImageIndex = Math.floor(Math.random() * 7) + 1;
+    } while (newImageIndex === currentImageIndex);
+
+    currentImageIndex = newImageIndex; 
+    return `img${newImageIndex}.PNG`; 
+}
+
+const fullscreenImage = document.getElementById("fullscreen-image");
+fullscreenImage.addEventListener("click", () => {
+    fullscreenImage.src = getRandomImage(); 
 });
